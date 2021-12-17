91c96d172fc3ac73af0de63c2ce4ef027a1e274b
Rodrigo Gonzalez <rodrigogonvaz@gmail.com>
2021-12-17 12:40:32 -0600

    Basic structure FrontEnd React

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+//1. IMPORT
+
+import React from 'react'
+
+
+
+//2. FUNCTION:
+const Router = () => {
+
+  return (
+    <>
+      Hola Mundo
+    </>
+  )
+}
+
+
+//3.EXPORT
+export default Router
