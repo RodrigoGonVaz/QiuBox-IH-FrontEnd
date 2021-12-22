00241be422626f3fa5a1c75ce5d34f64d6deb050
Rodrigo Gonzalez <rodrigogonvaz@gmail.com>
2021-12-22 00:16:55 -0600

    Nueva Home Page con estilo

--- src/components/Layout/NavLinks.js
+++ src/components/Layout/NavLinks.js
@@ -17,12 +17,15 @@ const NavLinks = () => {
               </li>
               <li>
                 <Link className="text-rosamx-600 hover:text-rosamx-700" to="/artesanias">
-                  Artesanias
+                  Artesanias 🎨
                 </Link>
               </li>
               <li>
-                <Link className="text-rosamx-600 hover:text-rosamx-700" to="#">
+                <Link className="flex flex-row text-rosamx-600 hover:text-rosamx-700" to="#">
                   Qiubox
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+</svg>
                 </Link>
               </li>
             </ul>
